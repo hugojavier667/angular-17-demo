c595ad8cc6d8d43e848430daf898b6d434e9a08b
Hugo J. Peña <hpena@essentialaccessibility.com>
2024-03-01 00:07:54 -0500

    Second commit

--- src/app/components/signals-with-rx-js-demo/signals-with-rx-js-demo.component.ts
+++ src/app/components/signals-with-rx-js-demo/signals-with-rx-js-demo.component.ts
@@ -1,6 +1,7 @@
 import { Component, Signal, WritableSignal, computed, signal } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { CountriesService, ICountry } from '../../services/countries.service';
+import { Observable, of, switchMap, take } from 'rxjs';
 
 @Component({
   selector: 'app-signals-with-rx-js-demo',
@@ -14,6 +15,8 @@ export class SignalsWithRxJsDemoComponent {
   public search: WritableSignal<string> = signal('');
   public filteredCountries: Signal<ICountry[]>;
 
+  public search$: Observable<string>;
+
   constructor(private countriesService: CountriesService) {
     this.countries = toSignal(countriesService.getAll(), { initialValue: [] });
 
@@ -21,12 +24,36 @@ export class SignalsWithRxJsDemoComponent {
       return this.countries().filter((country: ICountry): boolean => !this.search() || !!country.name.common.match(this.search()))
     })
 
-    toObservable(this.search).subscribe((serachTerm: string): void => {
+    this.search$ = toObservable(this.search);
+    
+    this.search$.subscribe((serachTerm: string): void => {
       console.log('Search term changed: ', serachTerm);
     })
   }
 
   public filterByName(event: Event): void {
     this.search.set((event.target as HTMLInputElement).value);
+  }
+
+  /**
+   * An example of a method that needs to read an obervable to execute some other logic
+   */
+  public someMethod(): void {
+    this.search$.pipe(
+      take(1),
+      switchMap((search: string) => {
+        // do something else like calling an API
+        return of();
+      })
+    ).subscribe();
+  }
+
+  /**
+   * An example of a method that needs to read a signal to execute some other logic
+   */
+  public someOtherMethod(): void {
+    const searchValue: string = this.search();
+
+    // do something
   }
 }
